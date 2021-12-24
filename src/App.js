2eec131e1da08b061f6819d02b219334a487a658
SiddhantGupta1 <sg4714@srmist.edu.in>
2021-12-24 19:24:18 +0530

    Styling of the editor done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import Editor from './components/Editor';
 
 function App() {
+
+  const [html, setHtml] = useState('')
+  const [css, setCss] = useState('')
+  const [js, setJs] = useState('')
+
   return (
     <>
     <div className="window top-window">
-      <Editor />
-      <Editor />
-      <Editor />
+      <Editor editorTitle='HTML' language='xml' value={html} onChange={setHtml} />
+      <Editor editorTitle='CSS' language='css' value={css} onChange={setCss} />
+      <Editor editorTitle='JS' language='javascript' value={js} onChange={setJs} />
     </div>
     <div className="window">
       <iframe 
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -5,11 +5,38 @@ import 'codemirror/mode/xml/xml'
 import 'codemirror/mode/javascript/javascript'
 import 'codemirror/mode/css/css'
 import {Controlled as ControlledEditor} from 'react-codemirror2'
+import { MdOutlineOpenInFull } from 'react-icons/md'
 
-const Editor = () => {
+const Editor = (props) => {
+    const{
+        editorTitle,
+        language,
+        value,
+        onChange,
+    } = props;
+    const handleChange = (editor, data, value) => {
+        onChange(value)
+    }
     return (
-        <div>
-            Editor
+        <div className='editor-container'>
+            <div className='editor-title'>
+                {editorTitle}
+                <button className='editor-btn'>
+                    <MdOutlineOpenInFull color='white' />
+                </button>
+            </div>
+            <ControlledEditor 
+                onBeforeChange={handleChange}
+                value={value}
+                className='code-mirror-editor'
+                options ={{
+                    lineWrapping: true,
+                    lint: true,
+                    mode: language,
+                    theme: 'material',
+                    lineNumbers: true,
+                }}
+            />
         </div>
     )
 }
